d3addf40ec85e9acb86b615168aaeee1a399e556
chendi <bbigcd@163.com>
2016-12-02 09:06:06 +0800

    Day_02

---
+++ Swift_Day_01.playground/Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+print("Hello, Wrold!")
+
+
+// 编译器自动推断类型
+var myVarialble = 42
+myVarialble = 50
+let myConstant = 42
+
+let implicitInteger = 70
+let implicitDouble = 70.0
+let explicitDouble : Double = 70
+
+// 练习：创建一个常量，显示指定类型为Float并指定初始值为4
+
+let implicitFloat : Float = 4
+
+
+
+
+
+
